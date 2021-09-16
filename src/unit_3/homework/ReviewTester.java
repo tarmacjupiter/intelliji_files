cc2cccb42f94179d03306a2680b4542f40edaf92
Anthony Behery <anthonybehery@Davids-MacBook-Air.local>
2021-09-16 15:16:06 -0500

    ReveiwTester

--- src/unit_3/homework/ReviewTester.java
+++ src/unit_3/homework/ReviewTester.java
@@ -1,4 +1,19 @@
 package unit_3.homework;
 
 public class ReviewTester {
+    public static void main(String[] args) {
+        Review kg = new Review("Ken Goettsche", "Olive Garden", 1);
+        Review cn = new Review("Charles Nichols", "Vini's", 5);
+        Review sd = new Review("Shannon Denna", "Panera", 3);
+        Review jm = new Review("Jerry Moon", "Kura", 5);
+
+        System.out.println(kg);
+        System.out.println();
+        System.out.println(cn);
+        System.out.println();
+        System.out.println(sd);
+        System.out.println();
+        System.out.println(jm);
+    }
 }
+
