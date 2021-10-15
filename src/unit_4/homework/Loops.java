83551708b944f1b8836387368087b7a1572177a6
Anthony G Behery <behery6194@students.d211.org>
2021-10-14 19:39:45 -0500

    Loops Class finished

--- src/unit_4/homework/Loops.java
+++ src/unit_4/homework/Loops.java
@@ -7,15 +7,45 @@ public class Loops {
        as well. Use this method header:
         */
     public static void everyOtherLetterPyramid(String str){
-        for(int i = str.length(); i > 0; i -= 2){
-//            System.out.print(str.charAt(i));
-            for(int j = 1; j < i; j++){
-                System.out.print(str.charAt(j));
+//        for(int i = str.length(); i > 0; i -= 2){
+////            System.out.print(str.charAt(i));
+//            for(int j = 1; j < i; j++){
+//                System.out.print(str.charAt(j));
+//            }
+//            System.out.println();
+//        }
+
+        for(int i = 0; i < str.length(); i++){
+            for(int j = 0; j < i; j+=2){
+                System.out.printf("%c ", str.charAt(j));
             }
-            System.out.println();
+            System.out.printf("%c\n", str.charAt(i));
         }
     }
+
+    //String Builder :/
+
+    public static void everyOtherWordBackwards(String str){
+        /*
+        This is not my code, I looked this up. I can sorta understand how it works, but I've never used
+        StringBuilder before. 
+        */
+
+        StringBuilder sb = new StringBuilder();
+
+        String[] words = str.split(" "); //step 1
+
+        for(String word : words){
+            String reversed = new StringBuilder(word).reverse().toString();
+            sb.append(reversed + " ");
+        }
+        System.out.println(sb.toString().trim());
+    }
+
     public static void main(String[] args) {
+
+        everyOtherWordBackwards("hello there how are you!");
+
 
         everyOtherLetterPyramid("FremdVikings");
         System.out.println();
