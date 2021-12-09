9e1edd0d77ca1b33ba9daad4af56520f9dac967c
Anthony G Behery <behery6194@students.d211.org>
2021-12-08 19:16:20 -0600

    Scores functionality

--- src/unit_6/lab2/TriviaTester2.java
+++ src/unit_6/lab2/TriviaTester2.java
@@ -13,28 +13,48 @@ public class TriviaTester2 {
         Questions2[] arr = game.getRandomizedQuestions();
         String[] arrAnswers = game.getAnswersForQuestions();
 
+        // Scores, update during game
+        int correct = 0;
+        int wrong = 0;
+
+        //Total correct to divide by later
+        int answeredQuestions = 0;
+
         for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
             String input = sb.nextLine().toLowerCase();
             String answer = arrAnswers[i].toLowerCase();
+            answeredQuestions++;
 
             if(input.equals(answer)){
                 System.out.println("This is the right answer!\n");
+                correct++;
             } else {
                 System.out.println("This is the wrong answer!\n");
+                System.out.println("The right answer was:\n\t" + arrAnswers[i] + "\n");
+                wrong++;
             }
 
-            System.out.println("Do you want to continue?\n('Yes' or 'No')");
+            System.out.println("Your current correct is:\n\t" + correct);
+
+            System.out.println("Do you want to continue?\n('Y' or 'N')");
             String continueStatus = sb.nextLine().toLowerCase();
-            while(!continueStatus.equals("yes")){
-                if(continueStatus.equals("no")){
+            while(!continueStatus.equals("y")){
+                if(continueStatus.equals("n")){
                     System.out.println("Thank you for playing!");
+                    System.out.println("Your final score is\n\t\t" + correct + "/" + answeredQuestions);
+                    System.out.println("You got " + correct + " answers right");
+                    System.out.println("You got " + wrong + " answers wrong");
                     return;
                 }
-                System.out.println("This is not a valid input!\nPlease type ('Yes' or 'No')");
+                System.out.println("This is not a valid input!\nPlease type ('Y' or 'N')");
                 continueStatus = sb.nextLine().toLowerCase();
             }
         }
+        System.out.println("Your final score is\n\t\t" + correct + "/" + answeredQuestions);
+        System.out.println("You got " + correct + " answers right");
+        System.out.println("You got " + wrong + " answers wrong");
+    }
 
 //        for(int i = 0; i < game.getNumOfQuestions(); i++){
 //            int randomNum = random.nextInt((game.getNumOfQuestions() - 1) + 1 - 0) + 0;
@@ -61,7 +81,6 @@ public class TriviaTester2 {
 //            }
 //        }
 
-    }
 }
 
 
