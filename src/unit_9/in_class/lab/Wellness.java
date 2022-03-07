ef2e71f74833791aedfe9d1b27072694d6a47400
tarmacjupiter <beheryanthony@gmail.com>
2022-03-07 09:14:54 -0600

    workoutplan class

---
+++ src/unit_9/in_class/lab/Wellness.java
@@ -0,0 +1,35 @@
+package unit_9.in_class.lab;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Wellness extends Workout {
+    private int numStretches;
+
+    // Constructor
+    public Wellness(String name, int workoutNum, int duration, int numStretches) {
+        super(name, workoutNum, duration);
+        this.numStretches = numStretches;
+    }
+
+    //toString method
+    public String toString(){
+        return getWorkoutNum() + ":\t" + getName() + "\t" + getDuration();
+    }
+
+    // Generates a random number between [5,10] representing the calories burned per stretch
+    // Calculates and returns total calories (rounded to whole number) by taking the product of the calories per stretch and the number of stretches
+    public int startExercise(){
+        int random = ThreadLocalRandom.current().nextInt(5, 10 + 1);
+        return numStretches / random;
+    }
+
+    // Getters and Setters
+
+    public int getNumStretches() {
+        return numStretches;
+    }
+
+    public void setNumStretches(int numStretches) {
+        this.numStretches = numStretches;
+    }
+}
---
+++ src/unit_9/in_class/lab/WorkoutPlan.java
@@ -0,0 +1,13 @@
+package unit_9.in_class.lab;
+
+public class WorkoutPlan {
+    private int completedWorkouts;
+    private int skippedWorkouts;
+    private int currentWorkoutWeek;
+    private int nextWorkout;
+    private int burnedCalories;
+    private int minOfExercise;
+    private Workout[][] workouts;
+
+
+}
